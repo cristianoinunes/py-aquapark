8be533cd6b626961d71509aa01825cbb5994253b
cristianoinunes <cristianonunes_rs@hotmail.com>
2025-09-14 15:42:37 -0300

    Solved challenge update 2

--- app/main.py
+++ app/main.py
@@ -1,51 +1,81 @@
+from abc import ABC, abstractmethod
+from typing import Optional, Type
+
+
+class IntegerRange:
+    def __init__(self, min_value: int, max_value: int) -> None:
+        self.min_value: int = min_value
+        self.max_value: int = max_value
+
+    def __get__(self, instance: Optional[object],
+                owner: Type) -> Optional[int]:
+        return instance.__dict__.get(self._name) if instance else None
+
+    def __set__(self, instance: object, value: int) -> None:
+        if (value < self.min_value
+                or value > self.max_value):
+            raise ValueError(f"Value {value} "
+                             f"is out of range ({self.min_value}, "
+                             f"{self.max_value})")
+        instance.__dict__[self._name] = value
+
+    def __set_name__(self, owner: Type, name: str) -> None:
+        self._name = name
+
+
+class SlideLimitationValidator(ABC):
+    @abstractmethod
+    def __init__(self, age: int, weight: int, height: int) -> None:
+        self.age: int = age
+        self.weight: int = weight
+        self.height: int = height
+
+
+class ChildrenSlideLimitationValidator(SlideLimitationValidator):
+    age: IntegerRange
+    height: IntegerRange
+    weight: IntegerRange
+
+    age = IntegerRange(4, 14)
+    height = IntegerRange(80, 120)
+    weight = IntegerRange(20, 50)
+
+    def __init__(self, age: int, weight: int, height: int) -> None:
+        super().__init__(age, weight, height)
+
+
+class AdultSlideLimitationValidator(SlideLimitationValidator):
+    age: IntegerRange
+    height: IntegerRange
+    weight: IntegerRange
+
+    age = IntegerRange(14, 60)
+    height = IntegerRange(120, 220)
+    weight = IntegerRange(50, 120)
+
+    def __init__(self, age: int, weight: int, height: int) -> None:
+        super().__init__(age, weight, height)
+
+
 class Visitor:
-    def __init__(self, name: str, age: int, height: int, weight: int) -> None:
-        self.name = name
-        self.age = age
-        self.height = height
-        self.weight = weight
-
-
-class ChildrenSlideLimitationValidator:
-    @staticmethod
-    def validate(visitor: Visitor) -> bool:
-        if visitor.age < 4:
-            return False
-        if visitor.height < 80:
-            return False
-        if visitor.weight < 20:
-            return False
-        if visitor.age > 14:
-            return False
-        if visitor.height > 120:
-            return False
-        if visitor.weight > 50:
-            return False
-        return True
-
-
-class AdultSlideLimitationValidator:
-    @staticmethod
-    def validate(visitor: Visitor) -> bool:
-        if visitor.age < 14:
-            return False
-        if visitor.height < 120:
-            return False
-        if visitor.weight < 50:
-            return False
-        if visitor.age > 60:
-            return False
-        if visitor.height > 220:
-            return False
-        if visitor.weight > 120:
-            return False
-        return True
+    def __init__(self, name: str, age: int, weight: int, height: int) -> None:
+        self.name: str = name
+        self.age: int = age
+        self.weight: int = weight
+        self.height: int = height
 
 
 class Slide:
-    def __init__(self, name: str, limitation_class: str) -> None:
-        self.name = name
-        self.limitation_class = limitation_class
+    def __init__(self, name: str,
+                 limitation_class:
+                 Type[SlideLimitationValidator]) -> None:
+        self.name: str = name
+        self.limitation_class: (
+            Type)[SlideLimitationValidator] = limitation_class
 
     def can_access(self, visitor: Visitor) -> bool:
-        return self.limitation_class.validate(visitor)
+        try:
+            self.limitation_class(visitor.age, visitor.weight, visitor.height)
+            return True
+        except ValueError:
+            return False
